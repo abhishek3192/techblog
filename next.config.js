321502dc06a23dacaeb9525167a1d28c2e96128d
abhishek3192 <abi.shrma92@gmail.com>
2023-08-29 22:27:00 +0530

    changing next config file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-const nextConfig = (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return {
-      reactStrictMode: true,
-      env: {
-        USRENAME: process.env.USRENAME,
-        PASSWORD: process.env.PASSWORD,
-        CLUSTERNAME: process.env.CLUSTERNAME,
-        DATABASE: process.env.DATABASE,
-      },
-    };
-  }
+const nextConfig = () => {
   return {
-    reactStrictMode: true,
     env: {
       USRENAME: process.env.USRENAME,
       PASSWORD: process.env.PASSWORD,
