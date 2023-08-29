9207e1188f5109a500080a7b4a97f7f717a3d24e
abhishek3192 <abi.shrma92@gmail.com>
2023-08-29 09:58:09 +0530

    minor change

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = (phase) => {
   return {
     reactStrictMode: true,
     env: {
-      mongodb_username: process.env.mongodb_username,,
+      mongodb_username: process.env.mongodb_username,
       mongodb_password: process.env.mongodb_password,
       mongodb_clustername: process.env.mongodb_clustername,
       mongodb_database: process.env.mongodb_database,
