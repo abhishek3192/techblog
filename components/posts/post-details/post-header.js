6427c2c0074283e97e7d5cd52e73b410732c0772
abhishek3192 <abi.shrma92@gmail.com>
2023-08-30 16:27:40 +0530

    some css changes

--- components/posts/post-details/post-header.js
+++ components/posts/post-details/post-header.js
@@ -5,7 +5,7 @@ import classes from "./post-header.module.css";
 const PostHeader = ({ title, image }) => {
   return (
     <header className={classes.header}>
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <Image src={image} alt={title} width={200} height={150} priority />
     </header>
   );
